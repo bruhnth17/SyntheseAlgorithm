5d6fcc63010fd3b35d7255598b068ca0d8976e92
= <tom-95@hotmail.de>
2017-09-01 11:55:53 +0200

    communication between depContainer and formContainer

--- src/components/dependenciesContainer/dependencies-container.js
+++ src/components/dependenciesContainer/dependencies-container.js
@@ -1,3 +1,25 @@
+import {inject} from 'aurelia-framework';
+import {EventAggregator} from 'aurelia-event-aggregator';
+
+@inject(EventAggregator)
 export class DependenciesContainer {
-  constructor() {}
+  constructor(eventAggregator) {
+    this.ea = eventAggregator;
+    this.subscribe();
+  }
+
+  subscribe() {
+    this.ea.subscribe('numbers', response => {
+      this.adjustDependencieOptions(response.attr.id, response.dep.id);
+    });
+  }
+
+  adjustDependencieOptions(attr, dep) {
+    console.log(attr, dep);
+  }
+
+
+  removeRotate() { //go Back
+    document.getElementsByClassName('form')[0].classList.remove('rotate');
+  }
 }
--- src/components/formContainer/form-container.js
+++ src/components/formContainer/form-container.js
@@ -1,18 +1,13 @@
-import {bindable} from 'aurelia-framework';
+import {inject} from 'aurelia-framework';
+import {EventAggregator} from 'aurelia-event-aggregator';
 
+@inject(EventAggregator)
 export class FormContainer {
 
-  constructor() {
+  constructor(eventAggregator) {
     this.headline = 'Schritt 1: Anzahl Attribute & Abhängigkeiten';
-    this.numAttr = [
-      {id: 3, label: '3'},
-      {id: 4, label: '4'},
-      {id: 5, label: '5'},
-      {id: 6, label: '6'},
-      {id: 7, label: '7'},
-      {id: 8, label: '8'}
-    ];
-    this.numDep = [
+    this.ea = eventAggregator;
+    this.numAttr = this.numDep =  [
       {id: 3, label: '3'},
       {id: 4, label: '4'},
       {id: 5, label: '5'},
@@ -25,11 +20,9 @@ export class FormContainer {
     this.nameAttr;
   }
 
-  log() {
-    console.log(this.nameAttr);
-  }
-
   rotate() {
+    this.ea.publish('numbers', {'attr': this.selectedAttr, 'dep': this.selectedDep});
+
     document.getElementsByClassName('form')[0].classList.add('rotate');
   }
 }

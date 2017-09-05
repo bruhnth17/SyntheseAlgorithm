f365bb26144fe2187835b0f118913a3fbe00046e
= <tom-95@hotmail.de>
2017-09-05 15:08:04 +0200

    started building dependency json

--- src/components/dependenciesContainer/dependencies-container.js
+++ src/components/dependenciesContainer/dependencies-container.js
@@ -20,8 +20,8 @@ export class DependenciesContainer {
 
   /**
    * Adjust form for entering dependencies
-   * @param {*number of attributes shown in form} attr
-   * @param {*number of dependencies shown in form} dep
+   * @param {number of attributes shown in form} attr
+   * @param {number of dependencies shown in form} dep
    */
   adjustDependencyOptions(attr, dep) {
     //console.log('adjustDependencyOptions');
@@ -53,5 +53,38 @@ export class DependenciesContainer {
 
   removeRotate() {
     document.getElementsByClassName('form')[0].classList.remove('rotate');
+  }
+
+  /**
+   * Create JSON Object with the filled in Dependencies
+   * Example:
+   * AEH -> BD :: {id : {left: ['A', 'E', 'H'], right: ['B', 'D'] }}
+   */
+  evaluateDependencies() {
+    let count = 0;
+    let result = {};
+    let dependencies = document.getElementsByClassName('dependency');
+
+    for (let i = 0; i < dependencies.length; i++) {
+      let arrayLeft = [];
+      let arrayRight = [];
+      
+      let leftInputs = dependencies[i].firstChild.childNodes;
+      for (let j = 0; j < leftInputs.length; j++) {
+        if (leftInputs[j].firstChild.checked) {
+          arrayLeft.push(leftInputs[j].lastChild.innerHTML);
+        }
+      }
+
+      let rightInputs = dependencies[i].firstChild.childNodes;
+      for (let j = 0; j < rightInputs.length; j++) {
+        if (rightInputs[j].firstChild.checked) {
+          arrayRight.push(rightInputs[j].lastChild.innerHTML);
+        }
+      }
+      result[count.toString()] = {'left': arrayLeft, 'right': arrayRight};
+      count++;
+    }
+    console.log(result);
   }
 }
